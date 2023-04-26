17f45ebf49ef7d43dac95063ad34c089a7cb5225
CaVaiRs <Unlimi.net@mail.ru>
2023-04-26 17:16:57 +0300

    add rand pass

--- create_sip.py
+++ create_sip.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import json
 import time
+import random
+import string
  
 # Указываем полный путь к geckodriver.exe на вашем ПК.
 # EXE_PATH = r'pa\to\chromedriver.exe'
@@ -30,30 +32,51 @@ driver = webdriver.Chrome(service=ser, options=op,)
 
 op.add_argument('--allow-running-insecure-content')
 op.add_argument('--ignore-certificate-errors')
+print('Выберите сервер:')
+print('1 - Глобальный сервер')
+print('2 - Русский сервер ')
+server_enter = int(input('Ваш выбор : '))
+if server_enter == 1:
+   driver.get("https://sip.bas-ip.com")
+elif server_enter == 2:
+   driver.get("https://ru.sip.bas-ip.com")
+ 
 
-driver.get("https://sip.bas-ip.com")
 assert "BAS-IP" in driver.title
-# element = driver.find_element_by_id ('appbundle_login_email')
-driver.find_element(By.ID, "appbundle_login_email").send_keys("skandinavskybulvar.19@gmail.com")
-driver.find_element(By.ID, "appbundle_login_password").send_keys("A101a101")
+
+email_enter = input('Введите Email от личного кабинета ')
+password_enter = input('Введите Пароль от личного кабинета ')
+
+driver.find_element(By.ID, "appbundle_login_email").send_keys(email_enter)
+driver.find_element(By.ID, "appbundle_login_password").send_keys(password_enter)
 driver.find_element(By.CLASS_NAME, "btn-success").click()    
     
          
-start_number = 1
-count = 628
+start_number = 629  # указываем с какой квартиры начинать
+count = int(input('Укажите колчисество квартир '))  # указываем количество квартир
+chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+number_pass = 1
+length = 3  # длина пароля  (Плюс три символа)
+number_pass = int(number_pass)
+length = int(length)
 
 for i in range(count):
-    string = "19kv"
+    my_string = "19kv"
     number = start_number + i
-    driver.get("https://sip.bas-ip.com/family/sip-numbers/new")
-    driver.find_element(By.ID, "appbundle_sipnumber_name").send_keys(string + str(number))
-    driver.find_element(By.ID, "appbundle_sipnumber_password").send_keys('A123456a')
+    if server_enter == 1:
+        driver.get("https://sip.bas-ip.com/family/sip-numbers/new")
+    elif server_enter == 2:
+        driver.get("https://ru.sip.bas-ip.com/family/sip-numbers/new")    
+    driver.find_element(By.ID, "appbundle_sipnumber_name").send_keys(my_string + str(number))
+    for n in range(number_pass):
+        password = str(random.randint(0, 9))
+        password += random.choice(string.ascii_lowercase)
+        password += random.choice(string.ascii_uppercase)
+        print(password)
+        for x in range(length):
+            password += random.choice(chars)
+    driver.find_element(By.ID, "appbundle_sipnumber_password").send_keys(password)
     driver.find_element(By.CLASS_NAME, "btn-success").click()
-    
-    
-
-
-
 
 
 print(driver.title)
@@ -63,5 +86,19 @@ driver.quit()
 
 
 
-# for string in generate_string(start_string, count):
-#     print(string)
+
+
+
+
+    #         password += str(random.randint(0, 9))
+    #         print(password)
+
+    # # Добавить одну случайную маленькую букву
+    #         password += random.choice(string.ascii_lowercase)
+    #         print(password)
+
+    # # Добавить одну случайную большую букву
+    #         password += random.choice(string.ascii_uppercase)
+
+    # # Добавить остальные случайные символы
+    #         password += ''.join(random.sample(string.ascii_letters + string.digits, length - 3))
---
+++ generate_pass.py
@@ -0,0 +1,16 @@
+import random
+
+chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+number = 1
+length = 6
+number = int(number)
+length = int(length)
+for n in range(number):
+    password =''
+    for i in range(length):
+        password += random.choice(chars)
+    print(password)
+
+
+
+
